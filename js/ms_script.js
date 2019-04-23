ece16327df5e9016cd4aa4b96bc43f0ce787e649
ValentinaKocharyan <valentinaqocharyan@gmail.com>
2019-04-23 13:13:13 -0700

    issue-2: change active class with toggle method

--- js/ms_script.js
+++ js/ms_script.js
@@ -1,6 +1,5 @@
 $('document').ready(function () {
     $('.js-dropdown-toggle-btn').on('click', function () {
-        let multiSelectDropdown = $(this).closest('.js-multiselect-group');
-        multiSelectDropdown.hasClass('active') ? multiSelectDropdown.removeClass('active') : multiSelectDropdown.addClass('active');
+        $(this).closest('.js-multiselect-group').toggleClass('active');
     })
 });

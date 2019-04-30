65a59310d1c22ef4f82bd3712525353b9af2504f
karenengineer <karengevorgyan912@mail.ru>
2019-04-30 07:36:15 +0400

    isssue9 protected  and function

--- js/ms_script.js
+++ js/ms_script.js
@@ -1,306 +1,308 @@
-$('document').ready(function () {
-    let itemsArray = [
-        {
-            id : 1,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 2,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 3,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 4,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 5,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 6,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 7,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 8,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 9,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 10,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 11,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 12,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 13,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 14,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 15,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 16,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 17,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 18,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 19,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 20,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 21,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 22,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 23,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 24,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 25,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 26,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 27,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 28,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 29,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 30,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 31,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 32,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 33,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 34,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 35,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 36,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 37,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 38,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 39,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 40,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 41,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 42,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 43,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 44,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 45,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 46,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 47,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 48,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        },
-        {
-            id : 49,
-            img_url : 'img/ndsu.png',
-            name : 'text'
-        },
-        {
-            id : 50,
-            img_url : 'img/logo1.jpg',
-            name : 'New Balance'
-        },
-        {
-            id : 51,
-            img_url : 'img/logo2.jpg',
-            name : 'Adidas'
-        },
-        {
-            id : 52,
-            img_url : 'img/logo3.jpg',
-            name : 'Nike'
-        }
-    ];
-    let selectedItemsArray = [];
-    let itemsOptions = {
-        limit : 20,
-        total : itemsArray.length,
-        offset : 0
-    };
-    let cycleLength = itemsOptions.limit <= itemsOptions.total ? itemsOptions.limit : itemsOptions.total;
-
-    getItemsList(itemsArray, itemsOptions.offset, cycleLength);
-
-    $('.js-dropdown-toggle-btn').on('click', function () {
-        $(this).closest('.js-multiselect-group').toggleClass('active');
-    });
-
-    $('.js-list-item').on('click', function() {
-        let currentItem = {
-            id : $(this).attr('data-id'),
-            img_url : $(this).find('.js-list-item-img').attr('src'),
-            name : $(this).find('.js-list-item-text').text()
-        };
-        let selectedExistedItem = $(`.js-selected-list-item[data-id="${currentItem.id}"]`);
-
-        $(this).hasClass('active') ? deleteFromSelectedList(selectedExistedItem, currentItem.id, selectedItemsArray) : addToSelectedList(currentItem, selectedItemsArray);
-        $(this).toggleClass('active');
-    });
-
-    let lastScrollTop = 0;
-
-    $('.js-items-list').on('scroll', function() {
-        let scrollTop = $(this).scrollTop();
-        if (scrollTop > lastScrollTop && scrollTop + $(this).innerHeight() >= this.scrollHeight){
-            if(itemsOptions.offset !== itemsOptions.total) {
-                itemsOptions.offset += cycleLength;
-                cycleLength = itemsOptions.limit <= itemsOptions.total - itemsOptions.offset ? itemsOptions.limit : itemsOptions.total - itemsOptions.offset;
-                getItemsList(itemsArray, itemsOptions.offset, cycleLength + itemsOptions.offset);
+$.fn.multiSelectPLugin = (function($) {
+    $('document').ready(function () {
+        let itemsArray = [
+            {
+                id : 1,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 2,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 3,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 4,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 5,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 6,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 7,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 8,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 9,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 10,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 11,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 12,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 13,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 14,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 15,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 16,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 17,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 18,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 19,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 20,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 21,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 22,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 23,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 24,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 25,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 26,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 27,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 28,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 29,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 30,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 31,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 32,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 33,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 34,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 35,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 36,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 37,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 38,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 39,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 40,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 41,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 42,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 43,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 44,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 45,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 46,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 47,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 48,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
+            },
+            {
+                id : 49,
+                img_url : 'img/ndsu.png',
+                name : 'text'
+            },
+            {
+                id : 50,
+                img_url : 'img/logo1.jpg',
+                name : 'New Balance'
+            },
+            {
+                id : 51,
+                img_url : 'img/logo2.jpg',
+                name : 'Adidas'
+            },
+            {
+                id : 52,
+                img_url : 'img/logo3.jpg',
+                name : 'Nike'
             }
-        }
-        lastScrollTop = scrollTop;
+        ];
+        let selectedItemsArray = [];
+        let itemsOptions = {
+            limit : 20,
+            total : itemsArray.length,
+            offset : 0
+        };
+        let cycleLength = itemsOptions.limit <= itemsOptions.total ? itemsOptions.limit : itemsOptions.total;
+
+        getItemsList(itemsArray, itemsOptions.offset, cycleLength);
+
+        $('.js-dropdown-toggle-btn').on('click', function () {
+            $(this).closest('.js-multiselect-group').toggleClass('active');
+        });
+
+        $('.js-list-item').on('click', function() {
+            let currentItem = {
+                id : $(this).attr('data-id'),
+                img_url : $(this).find('.js-list-item-img').attr('src'),
+                name : $(this).find('.js-list-item-text').text()
+            };
+            let selectedExistedItem = $(`.js-selected-list-item[data-id="${currentItem.id}"]`);
+
+            $(this).hasClass('active') ? deleteFromSelectedList(selectedExistedItem, currentItem.id, selectedItemsArray) : addToSelectedList(currentItem, selectedItemsArray);
+            $(this).toggleClass('active');
+        });
+
+        let lastScrollTop = 0;
+
+        $('.js-items-list').on('scroll', function() {
+            let scrollTop = $(this).scrollTop();
+            if (scrollTop > lastScrollTop && scrollTop + $(this).innerHeight() >= this.scrollHeight){
+                if(itemsOptions.offset !== itemsOptions.total) {
+                    itemsOptions.offset += cycleLength;
+                    cycleLength = itemsOptions.limit <= itemsOptions.total - itemsOptions.offset ? itemsOptions.limit : itemsOptions.total - itemsOptions.offset;
+                    getItemsList(itemsArray, itemsOptions.offset, cycleLength + itemsOptions.offset);
+                }
+            }
+            lastScrollTop = scrollTop;
+        });
     });
-});
+})(jQuery)
 
 
 function getItemsList(items, offset, iterationCount) {
